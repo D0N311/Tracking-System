af9e5e39fd38eb2b2bee00ea562f7d25d85de765
DON311 <palisan.235105@cdo.sti.edu.ph>
2024-04-26 10:37:36 +0800

    done

--- routes/api.php
+++ routes/api.php
@@ -25,10 +25,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('register', [RegisterController::class, 'register']);
 // Route::post('login', [LoginController::class, 'login']);
 Route::post('login', [LoginController::class, 'login'])->middleware('throttle:3,1');
