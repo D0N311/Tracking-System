9ce1de370dfeed1161e25ddd32329caaf40e24e5
DON311 <palisan.235105@cdo.sti.edu.ph>
2024-04-29 08:39:32 +0800

    done backend

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -9,28 +9,38 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\VerificationMail;
 use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
     //
     public function register(RegisterRequest $request): JsonResponse
     {
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] = $user->createToken('MyApp');
-        $success['name'] = $user->name;
-    
-        if (User::count() == 1) {
-            $role = Role::where('name', 'SuperAdmin')->first();
-            $user->roles()->attach($role->id);
-            $user->activated_at = now();
-            Mail::to($request->email)->send(new VerificationMail($user));
-            $user->save();
-        } else {
-            Mail::to($request->email)->send(new VerificationMail($user));
+        DB::beginTransaction();
+        try {
+            $input = $request->all();
+            $input['password'] = bcrypt($input['password']);
+            $user = User::create($input);
+            $success['token'] = $user->createToken('MyApp');
+            $success['name'] = $user->name;
+        
+            if (User::count() == 1) {
+                $role = Role::where('name', 'SuperAdmin')->first();
+                $user->roles()->attach($role->id);
+                $user->activated_at = now();
+                Mail::to($request->email)->send(new VerificationMail($user));
+                $user->save();
+            } else {
+                Mail::to($request->email)->send(new VerificationMail($user));
+            }
+            DB::commit();
+            return $this->sendResponse($success, 'User registered successfully.');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->sendError('User registration failed.', $e->getMessage(), 500);
         }
+      
     
-        return $this->sendResponse('User registered successfully.', []);
+        return $this->sendResponse($success, 'User registered successfully.');
     }
 }
